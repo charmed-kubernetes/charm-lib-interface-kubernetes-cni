530a1c288027dcfa5b2468339824ab2ecc3f6e35
George Kraft <george.kraft@canonical.com>
2023-09-13 16:04:23 -0500

    Address PR feedback

--- charms/interface_kubernetes_cni.py
+++ charms/interface_kubernetes_cni.py
@@ -1,17 +1,19 @@
 import hashlib
 import os
+from ops import CharmBase, Relation, Unit
+from typing import List, Optional
 
 
 class KubernetesCniProvides:
     """Implements the Provides side of the kubernetes-cni interface."""
 
-    def __init__(self, charm, endpoint, default_cni):
+    def __init__(self, charm: CharmBase, endpoint: str, default_cni: str):
         self.charm = charm
         self.endpoint = endpoint
         self.default_cni = default_cni
 
     @property
-    def cidr(self):
+    def cidr(self) -> Optional[str]:
         relation = self.default_relation
         if not relation:
             return None
@@ -21,7 +23,7 @@ class KubernetesCniProvides:
                 return cidr
 
     @property
-    def cni_conf_file(self):
+    def cni_conf_file(self) -> Optional[str]:
         relation = self.default_relation
         if not relation:
             return None
@@ -31,7 +33,7 @@ class KubernetesCniProvides:
                 return cni_conf_file
 
     @property
-    def default_relation(self):
+    def default_relation(self) -> Optional[Relation]:
         if self.default_cni:
             relations = [
                 relation
@@ -43,28 +45,28 @@ class KubernetesCniProvides:
         return relations[0] if relations else None
 
     @property
-    def relations(self):
+    def relations(self) -> List[Relation]:
         return self.charm.model.relations[self.endpoint]
 
-    def set_image_registry(self, registry):
+    def set_image_registry(self, registry) -> None:
         for relation in self.relations:
             relation.data[self.unit]["image-registry"] = registry
 
-    def set_service_cidr(self, service_cidr):
+    def set_service_cidr(self, service_cidr) -> None:
         for relation in self.relations:
             relation.data[self.unit]["service-cidr"] = service_cidr
 
-    def set_kubeconfig_hash_from_file(self, path):
+    def set_kubeconfig_hash_from_file(self, path) -> None:
         kubeconfig_hash = hash_file(path)
         for relation in self.relations:
             relation.data[self.unit]["kubeconfig-hash"] = kubeconfig_hash
 
     @property
-    def unit(self):
+    def unit(self) -> Unit:
         return self.charm.unit
 
 
-def hash_file(path):
+def hash_file(path) -> str:
     if not os.path.exists(path):
         return ""
     with open(path, "rb") as f:
